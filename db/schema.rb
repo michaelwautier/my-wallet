b774629f8d440d16ce2e15aaab87aae85fd237f2
Michaël Wautier <wautiermichael@gmail.com>
2023-10-08 15:16:42 +0400

    enable extension in db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_10_06_183344) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
